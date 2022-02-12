990b3e082fedd9ea6789837dcd033ae837351ce5
Jose Romary <joseromarybrandao@gmail.com>
2022-02-12 16:17:38 -0300

    salvando dados na mock api

--- src/components/drag_drop/index.js
+++ src/components/drag_drop/index.js
@@ -1,44 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { parse } from "papaparse";
 import { DataTable } from "../table";
 
+export function DragDrop() {
+  const [active, setActive] = useState(false);
+  const [contacts, setContacts] = useState([]);
 
-export function DragDrop(){
-    const [active, setActive] = useState(false);
-    const [contacts, setContacts] = useState([]);
+  useEffect(()=> {
+    if(contacts.length > 0)  {
+        axios.post("http://localhost:5000/contacts", contacts);
+    }else{
 
-    return(
-        <>
-        <div
-          className="dropCsv"
-          style={{ borderColor: active ? "green" : "black" }}
-          onDragEnter={() => {
-            setActive(true);
-          }}
-          onDragLeave={() => {
-            setActive(false);
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
-          }}
-          onDrop={(e) => {
-            e.preventDefault();
-            setActive(false);
+    }
+  });
+  
 
-            Array.from(e.dataTransfer.files)
-              .filter((file) => file.type === "text/csv")
-              .forEach(async (file) => {
-                const text = await file.text();
-                const result = parse(text, { header: true });
-                setContacts((existing) =>  [...existing, ...result.data ]);
-              });
-          }}
-        >
-          Upload CSV
-        </div>
+  console.log(contacts.length);
 
-        <DataTable contato={contacts}/>
+  return (
+    <>
+      <div
+        className="dropCsv"
+        style={{ borderColor: active ? "green" : "black" }}
+        onDragEnter={() => {
+          setActive(true);
+        }}
+        onDragLeave={() => {
+          setActive(false);
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          setActive(false);
 
-        </>
-    )
+          Array.from(e.dataTransfer.files)
+            .filter((file) => file.type === "text/csv")
+            .forEach(async (file) => {
+              const text = await file.text();
+              const result = parse(text, { header: true });
+              setContacts((existing) => [...existing, ...result.data]);
+            });
+          // updateBD()
+        }}
+      >
+        Upload CSV
+      </div>
+
+      <DataTable contato={contacts} />
+    </>
+  );
 }
